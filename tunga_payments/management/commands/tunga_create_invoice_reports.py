6afb313dd1c71371b1fd872b0c4233b9a92944f9
Remo Samuel Paul <remosam42@gmail.com>
2020-03-20 14:11:22 +0300

    changes to client survey reports and client invoices

--- tunga_payments/management/commands/tunga_create_invoice_reports.py
+++ tunga_payments/management/commands/tunga_create_invoice_reports.py
@@ -30,11 +30,11 @@ class Command(BaseCommand):
         for invoice in invoices:
             sheet_data = [str(invoice.created_at), str(invoice.number),
                           int(invoice.amount), str(invoice.full_title),
-                          str(invoice.project.title)]
+                          str(invoice.project.title),
+                          str(invoice.project.owner.display_name)]
             save_to_google_sheet(file_id, sheet_data)
 
         sheet_data = ['Date Created', 'Invoice Number',
                       'Amount', 'Invoice Name',
-                      'Project Name']
+                      'Project Name', 'Project Owner']
         save_to_google_sheet(file_id, sheet_data)
-
--- tunga_projects/management/commands/tunga_client_survey_reports.py
+++ tunga_projects/management/commands/tunga_client_survey_reports.py
@@ -29,35 +29,13 @@ class Command(BaseCommand):
         total_count = client_surveys.count()
 
         title = "Client Survey Report - %s" % (today.strftime("%B %Y"))
-        file_id = create_to_google_sheet_in_platform_updates(title)
-        missed = 0
-        filled = 0
+        file_id = '1_-SxkrrPk8uqwbz_Xe2sZurff9tYCN0sqTkVGJZDNS4'
 
         for client_survey in client_surveys:
             # print client_survey
             sheet_data = [str(client_survey.created_at),
                           str(client_survey.project.title),
                           str(client_survey.status).title()]
-            if client_survey.status == 'missed':
-                missed = missed + 1
-            elif client_survey.status == 'completed':
-                filled = filled + 1
             print(sheet_data)
             print(total_count)
-            save_to_google_sheet(file_id, sheet_data)
-
-        percentage_filled = (filled / total_count) * 100
-
-        save_to_google_sheet(file_id, ["", "Total Client Surveys", total_count],
-                             total_count + 1)
-        save_to_google_sheet(file_id, ["", "Total Filled Surveys", filled],
-                             total_count + 2)
-        save_to_google_sheet(file_id, ["", "Total Missed Surveys", missed],
-                             total_count + 3)
-        save_to_google_sheet(file_id,
-                             ["", "Percentage Filled",
-                              str(percentage_filled) + "%"],
-                             total_count + 4)
-
-        sheet_data = ['Date Created', 'Project', 'Status']
-        save_to_google_sheet(file_id, sheet_data)
+            save_to_google_sheet(file_id, sheet_data,index=2)
