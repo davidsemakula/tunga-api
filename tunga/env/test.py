894372e991f29565fb4efff25dd8f70b29c8f789
Remo Samuel Paul <remosam42@gmail.com>
2020-06-23 14:19:06 +0300

    adding test settings

---
+++ tunga/env/test.py
@@ -0,0 +1,17 @@
+import os
+
+from tunga.settings import *
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': ':memory:',
+  }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
+#     }
+# }
