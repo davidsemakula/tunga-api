22672d20b2c04f7398cb7ba42ac8b73088b2f75b
Remo Samuel Paul <remosam42@gmail.com>
2020-06-24 06:33:53 +0300

    change redis url

--- tunga/env/test.py
+++ tunga/env/test.py
@@ -15,3 +15,14 @@ DATABASES = {
 #         'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
 #     }
 # }
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
