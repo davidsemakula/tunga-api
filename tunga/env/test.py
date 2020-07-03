da64db7a0be3ec44fe4ad28fa04e35c3c67785a6
Remo Samuel Paul <remosam42@gmail.com>
2020-07-03 08:07:02 +0300

    added coverage xml

--- tunga/env/test.py
+++ tunga/env/test.py
@@ -17,13 +17,13 @@ DATABASES = {
 # }
 
 
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
+#
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://redis:6379",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
