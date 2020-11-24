9e807a5b30ec2dae961fffeca803f23fc18e99ce
Remo Samuel Paul <remosam42@gmail.com>
2020-11-24 18:06:23 +0300

    added check if user is in SSO and is invited to platform send an email granting them access instead of an invite

--- tunga_profiles/sso_helper.py
+++ tunga_profiles/sso_helper.py
@@ -40,8 +40,63 @@ def change_sso_user_password(user, old_password, new_password):
         'old_password': old_password,
         'new_password': new_password,
     }
-    sso_create_user_endpoint = SSO_TOKEN_URL + "users/%s/set_password/" % user.sso_uuid
-    response = requests.post(sso_create_user_endpoint, data=pay_load)
+    sso_create_user_endpoint = SSO_TOKEN_URL + "users/%s/change_password/" % user.sso_uuid
+    headers = {
+        'Authorization': "Bearer %s" % get_sso_access_token(user)
+    }
+    response = requests.post(sso_create_user_endpoint, data=pay_load,
+                             headers=headers)
     if response.status_code == HTTP_200_OK:
         return True
     return False
+
+
+def check_if_users_exists_in_sso(current_user, email):
+    sso_filter_user_by_email_endpoint = SSO_TOKEN_URL + "users/?email=%s" % requests.utils.quote(
+        email)
+    headers = {
+        'Authorization': "Bearer %s" % get_sso_access_token(current_user)
+    }
+    response = requests.get(sso_filter_user_by_email_endpoint, headers=headers)
+    if response.status_code == HTTP_200_OK:
+        return True
+    return False
+
+
+def change_user_access_in_sso(current_user, email, platform_role):
+    sso_filter_user_by_email_endpoint = SSO_TOKEN_URL + "users/?email=%s" % requests.utils.quote(
+        email)
+    headers = {
+        'Authorization': "Bearer %s" % get_sso_access_token(current_user)
+    }
+    response = requests.get(sso_filter_user_by_email_endpoint, headers=headers)
+    if response.status_code == HTTP_200_OK:
+        response_data = response.json()
+        count = response_data['count']
+        if count > 0:
+            uuid = response_data['results'][0]['id']
+            sso_update_user_endpoint = SSO_TOKEN_URL + "users/%s/" % uuid
+            payload = {
+                'access': ['platform', 'developpp'],
+                'platform_role': platform_role
+            }
+            update_user_response = requests.patch(sso_update_user_endpoint,
+                                                 data=payload,
+                                                 headers=headers)
+            if update_user_response.status_code == HTTP_200_OK:
+                return True
+        else:
+            return False
+
+    return False
+
+
+def get_sso_access_token(user):
+    sso_refresh_token_endpoint = SSO_TOKEN_URL + "token/refresh/"
+    pay_load = {
+        'refresh': user.sso_refresh_token
+    }
+    response = requests.post(sso_refresh_token_endpoint, data=pay_load)
+    if response.status_code == HTTP_200_OK:
+        return response.json()['access']
+    return ""
