972eca045774add3a308dd6d91821edf402ce260
Remo Samuel Paul <remosam42@gmail.com>
2020-12-08 17:07:33 +0300

    added extra user fields to export command

--- tunga_auth/management/commands/tunga_export_active_users.py
+++ tunga_auth/management/commands/tunga_export_active_users.py
@@ -2,6 +2,7 @@ import csv
 
 from django.core.management import BaseCommand
 
+from tunga.settings import TUNGA_URL
 from tunga_auth.models import TungaUser
 
 
@@ -19,11 +20,13 @@ class Command(BaseCommand):
             USER_TYPE_DEVELOPER: "Developer",
             USER_TYPE_PROJECT_OWNER: "Project Owner",
             USER_TYPE_PROJECT_MANAGER: "Project Manager",
+
         }
 
         users = TungaUser.objects.filter(is_active=True)
         row = ['Date', 'Last Login', 'First Name', ' Last Name', 'Email',
-               'Username', 'Role', 'Type', 'Active', 'Password']
+               'Username', 'Role', 'Type', 'Active', 'Password', 'Country', 'Zip Code', 'City', 'Street',
+               'Phone_number', 'Image']
         with open('tunga_users_active.csv', 'a') as csvFile:
             writer = csv.writer(csvFile)
             writer.writerow(row)
@@ -36,6 +39,13 @@ class Command(BaseCommand):
                      user.username,
                      user_type.get(user.type, None),
                      user.type,
-                     user.is_active, user.password])
+                     user.is_active, user.password,
+                     user.profile.country if user.profile else "",
+                     user.profile.postal_code if user.profile else "",
+                     user.profile.postal_code if user.profile else "",
+                     user.profile.city if user.profile else "",
+                     user.profile.street if user.profile else "",
+                     user.profile.phone_number if user.profile else "",
+                     "%s%s" % (TUNGA_URL, user.image.url)]) if user.image else ""
 
         csvFile.close()
