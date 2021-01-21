63c80afa986bbddc0d8527879a7977fe2f6940d5
marthamareal <marthamareal@.com>
2021-01-21 18:33:54 +0300

    create new permission

--- tunga_auth/permissions.py
+++ tunga_auth/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 from rest_framework.permissions import IsAdminUser, SAFE_METHODS, IsAuthenticated
 
 from tunga_auth.utils import get_session_visitor_email
+from tunga_utils.constants import USER_TYPE_PROJECT_MANAGER
 
 
 class IsAuthenticatedOrEmailVisitorReadOnly(permissions.BasePermission):
@@ -23,3 +24,9 @@ class IsAdminOrCreateOnly(permissions.BasePermission):
         if request.method == 'POST':
             return True
         return IsAdminUser().has_permission(request, view)
+
+
+class IsPmOrIsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        is_pm = request.user.type == USER_TYPE_PROJECT_MANAGER
+        return is_pm or IsAdminUser().has_permission(request, view)
